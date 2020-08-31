fb60e37b05ad382169094fff50846aca77a10393
Austin <pirateTurner1>
2020-08-31 00:45:04 -0600

    working on header

--- 01-JoesBB/js/main.js
+++ 01-JoesBB/js/main.js
@@ -1,4 +1,4 @@
 document.querySelector('#company').innerText = "Joe's Bed and Breakfast"
-Document.querySelector('#company > h2').innerText = "Best B&B in UTAH!"
+Document.querySelector('header , h2').innerText = "Best B&B in UTAH!"
 var name = window.prompt("what is your name?")
 document.write("Hey" + name + ", Welcome to Joe's bed and Breakfast Page!")
