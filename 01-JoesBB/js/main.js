b73b15550ae7c0bfdd8edd3f70dab1f9a6167ba8
Austin <pirateTurner1>
2020-08-30 18:48:11 -0600

    added changes to stylesheet

---
+++ 01-JoesBB/js/main.js
@@ -0,0 +1,3 @@
+//document.getElementById('company').innerHTML = "Joe's Bed and Breakfast"//
+document.querySelector('#company').innerText = "Joe's Bed and Breakfast"
+Document.querySelector('header > h2').innerText = "Best B&B in UTAH!"
