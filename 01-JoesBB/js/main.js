d13f277b3eb9061c88791707febc0ff8fa244fe3
Austin <pirateTurner1>
2020-08-30 23:12:05 -0600

    trying to fix

--- 01-JoesBB/js/main.js
+++ 01-JoesBB/js/main.js
@@ -1,20 +1,3 @@
-/*var heading = document.createElement("h1", "h2");
-heading.textcontent = "testing testing testing";
-heading.id = "title";
-//heading ...
-var mainDiv = document.createElement("div");
-mainDiv.id = "mainBox";
-var h1 = document.createElement("h1");
-h1.textContent = "testing testing testing";
-h1.id = "title";
-mainDiv.appendChild(h1);
-*/
-//document.getElementById('company').innerHTML = "Joe's Bed and Breakfast"//
-/*manipulating the DOM*/
-/*function setup() {
-    h1.createElement('h1', "Joe's Bed and Breakfast")
-    h2.createElement('h2', "the Best B&B in UTAH!")
-}*/
 document.querySelector('#company').innerText = "Joe's Bed and Breakfast"
 Document.querySelector('#company > h2').innerText = "Best B&B in UTAH!"
 var name = window.prompt("what is your name?")
