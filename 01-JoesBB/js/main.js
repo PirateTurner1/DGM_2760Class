b5db53476dd592dab20538122992fb489ebebfec
austin <55255361+PirateTurner1@users.noreply.github.com>
2020-09-01 04:01:52 -0600

    found the problem and fixed it.

--- 01-JoesBB/js/main.js
+++ 01-JoesBB/js/main.js
@@ -1,5 +1,9 @@
+/* manipulating the DOM by inserting elements*/
+document.getElementById('company').innerHTML = "Joe's Bed and Breakfast";
+document.getElementById('slogan').innerHTML = "Best B&B in UTAH!";
 
-document.querySelector('#company').innerText = "Joe's Bed and Breakfast"
-document.querySelector('#company > h2').innerText = "Best B&B in UTAH!"
+/*document.querySelector('#company').innerText = "Joe's Bed and Breakfast"*/
+/*document.querySelector('#company > h2').innerText = "Best B&B in UTAH!"*/
+
 /*var name = window.prompt("what is your name?")
 document.write("Hey" + name + ", Welcome to Joe's bed and Breakfast Page!")*/
