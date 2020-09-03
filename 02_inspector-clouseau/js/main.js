250c18f29cb63b24665c8601d2d3cf2e08ae018c
austin <55255361+PirateTurner1@users.noreply.github.com>
2020-09-03 13:25:54 -0600

    css adjustments

--- 02_inspector-clouseau/js/main.js
+++ 02_inspector-clouseau/js/main.js
@@ -1,18 +1,21 @@
 document.querySelector('header > h1').innerText = "inspector Clouseau"
-document.querySelector('header > h2').innerText = "This iz Chef Inspecteor Clouseau speaking on de pheaun"
+document.querySelector('header > h2').innerText = "This iez Chef Inspecteor Clouseau speaking on de pheaun"
 
 function reDisplay() {
 let windowSizes = `windows size is ${window.innerWidth} pixels wide and by ${window.innerHeight} pixels tall.`
 let offset = `window offset is ${window.screenX} from the left edge and ${window.screenY} pixels from the top of the display.`
 
-let myWindow = document.querySelector('#myWindow').innerText = `${windowSizes} '\n' ${offset}`
+let myWindow = document.querySelector('#myWindow').innerText = `${windowSizes} \n ${offset}`
 
-let myDocument = 'Document Title is ' +`${document.title}` + `this document was last updated on ${document.lastModified}` 
-let myDocument = document.querySelector("#myDocument").innerText = `${myDocument}`
+var txt2 = 'Document Title is ' + document.title + "<br>";
+    txt2 += 'this document was last updated on ' + document.lastModified + "<br>";
+    document.getElementById("myDocument").innerHTML = txt2;
 }
 reDisplay()
 
 /*
+let myDocument = 'Document Title is ' +`${document.title}` + `this document was last updated on ${document.lastModified}` 
+let myDocument = document.querySelector("#myDocument").innerText = `${myDocument}`
 function reDisplay() {
     var txt = 'windows size is ' + window.innerWidth + " width by " +window.innerHeight+"tall.<br>";
     txt += 'window offset is '+ window.screenX + " from the left edge and " + window.screenY + "from the top of the display.<br>";
