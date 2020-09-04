9c870853dfd73cc28d68183859084ca460dc23cf
austin <55255361+PirateTurner1@users.noreply.github.com>
2020-09-04 02:36:01 -0600

    started another assignment

---
+++ 03-guessingGame/js/main.js
@@ -0,0 +1,14 @@
+// pick a random number between 0 and 16
+
+const correct number = Math.floor(Math.random() * 15)
+
+console.log( the correct number is `${correctNumber}`)
+
+let guessed = false
+let totalGuesses = 0
+let gamerGuess = 0
+
+function evalGuess(){
+    totalGuesses += 1 
+    gamerGuess 
+}
