5cd441c35e6b1ec5f7b0a3303c6f391e751beb9d
austin <55255361+PirateTurner1@users.noreply.github.com>
2020-10-01 19:18:03 -0600

    finishing off and minor details

--- 06-nonSenseStory/js/main.js
+++ 06-nonSenseStory/js/main.js
@@ -2,7 +2,7 @@
 document.querySelector('header > h1').innerText = "The Nonsense Story";
 document.querySelector('header > h2').innerText = "String Manipulation";
 
-//giving the second h1-h2 header
+//giving the body an h1-h2 header.
 document.getElementById('head1').innerHTML = "The Tail of The Tortoise And The Hair.";
 document.getElementById('head2').innerHTML = "What I have learned is:";
 
@@ -19,24 +19,31 @@ function tellStory() {
     let verbString = document.querySelector('#verbs').value
     verbString.toLowerCase()
     verbArray = verbString.split(/\s+/)
-
-    let storyArea = `Once upon a time, There was a speedy ${nounArray[1]} who bragged about how fast he could ${verbArray[0]}. 
-    Tired of hearing him boast, the ${adjectiveArray[0]} ${nounArray[1]}, challenged him to a race.
-    All the ${nounArray[2]}s in the forest gathered to watch.
-    ${nounArray[0]} ${verbArray[0]} down the road for a while and then paused to rest.
-    He looked back at the ${nounArray[0]} and cried out,
-    'How do you expect to win this race when you are ${verbArray[0]}ing along at your ${verbArray[0]}, slow pace?'
-    The ${nounArray[0]} stretched himself out alongside the road and fell asleep, thinking,'There is plenty of time to relax.'
-    But the ${adjectiveArray[0]} ${nounArray[0]} ${verbArray[0]} and walked. He never, ever ${verbArray[0]}ed until he came to the finish line.
-    The ${nounArray[0]}s who were watching cheered so loudly for ${nounArray[0]}, they woke up the sleeping ${nounArray[0]}.
-    The ${adjectiveArray[0]} ${nounArray[0]} got back up and began to run again, but it was too late. the ${nounArray[0]} was over the line.
-    After that, the ${nounArray} always reminded himself,
-    'Do not brag about your lightning pace, for ${verbArray[0]} and ${adjectiveArray[0]} wins the race!'`
     
+    //the story with an array. (an array within arrays)
+    let storyArea = `Once upon a time, There was a ${adjectiveArray[0]} ${nounArray[0]} who ${adjectiveArray[1]} about how fast he could ${verbArray[0]}. 
+    Tired of hearing him boast, the ${adjectiveArray[2]} ${nounArray[1]}, challenged him to a race.
+    All the ${nounArray[2]}s in the ${nounArray[3]} gathered to watch.
+    The ${nounArray[0]} started off and ${verbArray[0]}ed down the ${nounArray[4]} for a while and then paused to rest.
+    The ${nounArray[0]} looked back at the ${adjectiveArray[2]} ${nounArray[1]} and ${verbArray[1]} out,
+    'How do you expect to win this race when you are ${verbArray[2]}ing along at your ${adjectiveArray[2]}, ${adjectiveArray[2]} pace?'
+    The ${nounArray[0]} stepped himself off alongside the ${nounArray[4]} and fell asleep, thinking,'There is plenty of time to relax.'
+    But the ${adjectiveArray[2]} and ${adjectiveArray[3]} ${nounArray[1]}, ${verbArray[2]}ed and ${verbArray[2]}ed. He never even stopped until he crossed the finish line.
+    The ${nounArray[2]}s who were watching cheered so loudly for ${nounArray[1]}, that they woke up the ${nounArray[0]}.
+    The ${nounArray[0]} got back up and began to ${verbArray[0]} again, but it was too late. The ${nounArray[1]} was over the line.
+    After that, the ${nounArray[0]} always ${verbArray[3]} himself,
+    'Do not ${adjectiveArray[1]} about your ${adjectiveArray[4]} pace, for ${adjectiveArray[2]} and ${adjectiveArray[3]} wins the race!'`
+    
+    //putting it all together.
     document.querySelector('#story').textContent = storyArea
 
 }
 
+//button action required. 
 document.querySelector('#tellStoryButton').onclick = tellStory
 
-console.log()
+// the nouns that were taken out were: Hare, turtle, animal, forest, road.
+// the adjectives that were taken out were: speedy, brag /or bragged, slow, steady, lightning.
+// the verbs that were taken out were: run /or ran, cried, walking /or walked, reminded.
+
+
