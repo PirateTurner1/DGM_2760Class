52423a3a3f3a5616ae4b5e10cd315461b3c05757
austin <55255361+PirateTurner1@users.noreply.github.com>
2020-10-07 14:42:39 -0600

    found problems and fixed them. made changes to all 3.

--- 07-pizzaForm/js/main.js
+++ 07-pizzaForm/js/main.js
@@ -7,43 +7,50 @@ document.getElementById('head1').innerHTML = "The Pizza  Order Form";
 document.getElementById('head2').innerHTML = "What I have learned is:";
 
 // setting the doe, i mean the crust
+let message
+
 const pizza = {
     crust: 'thin',
     size: 'large',
-    toppings: 'pepperoni',
-    veggieToppings: 'pineapple',
+    topping: 'pepperoni',
+    veggieTopping: 'pineapple',
 
     buildPizza: function () {
-        console.log ('building the pizza has been called for you!');
-        message = `we are baking a ${pizza.size} ${pizza.crust} crust with ${pizza.topping}, ${pizza.veggieTopping} and cheese just for you!`;
-        document.querySelector('#feedback').textContent = message;
+        console.log('buildPizza method has been called')
+        message = `We are baking a ${pizza.size} Pizza with ${pizza.crust} crust with ${pizza.topping} and ${pizza.veggieTopping} toppings. It will be ready in 20 minutes!`
+        document.querySelector('#feedback').textContent = message
     },
+
     shoppingList: () => {
-        let flour = 1;
-        if (pizza.crust === 'thick') flour *= 2;
-        else if (pizza.crust === 'large') flour *= 2;
-        else if (pizza.size === 'medium') flour = 1
-        
-        else if (pizza.size === 'small') flour /= 1;
-        //i will do the same for for large properties
-        message = `you will need to buy ${flour} cup(s) of flour and 1 pound of ${pizza.toppings}.`;
+        let flour = 1
+        if (pizza.crust === 'thick') flour *= 2
+        else if (pizza.size === 'small') flour /= 1
+        else if (pizza.size === 'large') flour *= 3
+        else if (pizza.crust === 'thin') flour /= 1
+        else if (pizza.crust === 'stuffed') flour *= 3
+        message = `You will need to have ${flour} cups of flour and 1-lb of ${pizza.topping} and 16-oz of ${pizza.veggieTopping}.`
         document.querySelector('#feedback').textContent = message
     }
-};// do this same thing for toppings and all others that have a pizza thing to it. 
-
-let thinButton = document.querySelector('#thin')
-thinButton.addEventListener('click', function(){
-    return pizza.crust = 'thin';
-})
-/*
-let buildButton = document.querySelector('#build') 
-buildButton.addEventListener('click, pizza.buildPizza ) 
-document.querySelector('#build')
-*/
-//what we would like to see as a code...
+}
+// RADIO BUTTONS
 document.querySelector('#thin').addEventListener('click', () => pizza.crust = 'thin')
-document.querySelector('#thick').addEventListener('click', ()=> pizza.crust = 'thick')    
-// do the same with toppings and size
-document.querySelector('#build').addEventListener('click', pizza.buildPizza)
-//hook up the shoppingList method, call it here. 
+document.querySelector('#thick').addEventListener('click', () => pizza.crust = 'thick')
+document.querySelector('#stuffed').addEventListener('click', ()=> pizza.crust = 'stuffed')
 
+document.querySelector('#sausage').addEventListener('click', () => pizza.topping = 'sausage') 
+document.querySelector('#pepperoni').addEventListener('click', () => pizza.topping = 'pepperoni')
+document.querySelector('#ham').addEventListener('click', () => pizza.topping = 'ham')
+document.querySelector('#chicken').addEventListener('click', () => pizza.topping = 'chicken')
+
+document.querySelector('#small').addEventListener('click', () => pizza.size = 'small')
+document.querySelector('#medium').addEventListener('click', ()=> pizza.size = 'medium')
+document.querySelector('#large').addEventListener('click', () => pizza.size = 'large')
+
+document.querySelector('#pineapple').addEventListener('click', () => pizza.veggieTopping = 'pineapple')
+document.querySelector('#mushrooms').addEventListener('click', () => pizza.veggieTopping = 'mushrooms')
+document.querySelector('#peppers').addEventListener('click', () => pizza.veggieTopping = 'peppers')
+document.querySelector('#olives').addEventListener('click', () => pizza.veggieTopping = 'olives')
+
+// ACTUAL BUTTONS
+document.querySelector('#build').addEventListener('click', pizza.buildPizza)
+document.querySelector('#shopping').addEventListener('click', pizza.shoppingList)
