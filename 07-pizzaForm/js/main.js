3b16232ff5866d824035932c9e473cc70c596425
austin <55255361+PirateTurner1@users.noreply.github.com>
2020-10-07 02:40:59 -0600

    working on it

--- 07-pizzaForm/js/main.js
+++ 07-pizzaForm/js/main.js
@@ -1,6 +1,6 @@
 // giving the top h1-h2 header.
-document.getElementById('#company').innerHTML = "Pizza Emporium";
-document.querySelector('header > h2').innerText = "Literal Objects"
+document.getElementById('#company').innerText = "Pizza Emporium";
+document.querySelector('header > h2').innerText = "Literal Objects";
 
 //giving the body an h1-h2 header.
 document.getElementById('head1').innerHTML = "Pizza Order Form";
@@ -11,11 +11,11 @@ const pizza = {
     crust: 'thin',
     size: 'large',
     toppings: 'pepperoni',
-    veggieToppings: 'pineapple';
+    veggieToppings: 'pineapple',
 
     buildPizza: function () {
         console.log ('building the pizza has been called for you!');
-        message = `Baking a ${pizza.size} pizza on a ${pizza.crust} crust with a ${pizza.topping} and cheese just for you!`;
+        message = `we are baking a ${pizza.size} ${pizza.crust} crust with ${pizza.topping}, ${pizza.veggieTopping} and cheese just for you!`;
         document.querySelector('#feedback').textContent = message;
     },
     shoppingList: () => {
@@ -24,12 +24,12 @@ const pizza = {
         else if (pizza.crust === 'large') flour *= 2;
         else if (pizza.size === 'medium') flour = 1
         
-        else (pizza.size === 'small') flour /= 1
+        else if (pizza.size === 'small') flour /= 1;
         //i will do the same for for large properties
-        message = `you will need to purchase ${flour} cups of flour and 1 lbs of ${pizza.toppings}. `;
+        message = `you will need to buy ${flour} cup(s) of flour and 1 pound of ${pizza.toppings}.`;
         document.querySelector('#feedback').textContent = message
     }
-}// do this same thing for toppings and all others that have a pizza thing to it. 
+};// do this same thing for toppings and all others that have a pizza thing to it. 
 
 
 
@@ -39,10 +39,9 @@ thinButton.addEventListener('click', function(){
     return pizza.crust = 'thin';
 })
 
-let buildButton = document.querySelector('#build')
-buildButton.addEventListener('click, function(){
-    return pizza.buildPizza()
-})
+let buildButton = document.querySelector('#build') 
+buildButton.addEventListener('click, pizza.buildPizza ) 
+document.querySelector('#build')
 
 //what we would like to see as a code...
 document.querySelector('#thin').addEventListener('click', () => pizza.crust = 'thin')
