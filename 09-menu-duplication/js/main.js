96fda5eff48659c0c97d934f8133094f3297b807
austin <55255361+PirateTurner1@users.noreply.github.com>
2020-10-21 14:35:39 -0600

    worked on all html,java,and css to finish chapter

--- 09-menu-duplication/js/main.js
+++ 09-menu-duplication/js/main.js
@@ -1,18 +1,25 @@
-
+// giving the top h1-h2 header.
+document.querySelector('header > h1').innerText = "Menu Duplication";
+document.querySelector('header > h2').innerText = "Media Query's";
 
 function duplicateMenu() {
-    // get all of the anchor elements from the top menu
+    //get all of the anchor elements from the top menu
     let topList = document.querySelectorAll('ul#primaryNavigation li a')
-
-    //create the new list of items from the button of the page.
+    // create the new list items for the bottom of the page
     let newList = document.createElement('ul')
 
     topList.forEach(menuItem => {
         let newLI = document.createElement('li')
         let newLink = document.createElement('a')
         newLink.setAttribute('href', menuItem.getAttribute('href'))
-        //'copy' the textContent from the uper menu to a new menu
-        // then appendChild to make then all appear in the DOM 
+        // 'copy' the textContent from upper menu to new menu 
+        let bottomNav = document.querySelector('#smallNavArea')
+        newLink.textContent = menuItem.textContent
+        console.log(menuItem.textContent)
+        // append children to make them appear in the DOM
+        newLI.appendChild(newLink)
+        newList.appendChild(newLI)
+        bottomNav.appendChild(newList)
 
     })
 }
