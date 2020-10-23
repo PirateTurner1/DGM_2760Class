35eeb782adafd5055658b824558153a5eb9731e8
austin <55255361+PirateTurner1@users.noreply.github.com>
2020-10-23 14:16:46 -0600

    added json file and started to work more on chapter 10

---
+++ 10- hotelBooking/js/main.js
@@ -0,0 +1,7 @@
+// giving the top h1-h2 header.
+document.querySelector('header > h1').innerText = "Hotel Booking";
+document.querySelector('header > h2').innerText = "Using JSON Data";
+
+//giving the body an h1-h2 header.
+document.getElementById('head1').innerHTML = "Hotel California";
+document.getElementById('head2').innerHTML = "What I have learned is:";
