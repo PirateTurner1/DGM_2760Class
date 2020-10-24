ecc3c70e06c347b284ed3cc3b9a57a8d00368ba9
austin <55255361+PirateTurner1@users.noreply.github.com>
2020-10-24 02:35:06 -0600

    working on javascript and JSON

--- 10- hotelBooking/js/main.js
+++ 10- hotelBooking/js/main.js
@@ -5,3 +5,16 @@ document.querySelector('header > h2').innerText = "Using JSON Data";
 //giving the body an h1-h2 header.
 document.getElementById('head1').innerHTML = "Hotel California";
 document.getElementById('head2').innerHTML = "What I have learned is:";
+
+//get api
+// promises, call backs, asyncs, awaits are similar in ways... please read!
+async function getHotelData() {
+try {
+    const response = await fetch('../hotel.JSON')
+    return await response.json() //this returns the JSON objects
+
+}catch (error) {
+    console.log(error)
+}
+
+}
