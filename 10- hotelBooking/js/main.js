b7dbad218bc3eebabd331b883b5970a52e2eea02
austin <55255361+PirateTurner1@users.noreply.github.com>
2020-10-28 04:30:28 -0600

    major fixes on details. added pictures.

--- 10- hotelBooking/js/main.js
+++ 10- hotelBooking/js/main.js
@@ -6,11 +6,10 @@ document.querySelector('header > h2').innerText = "Using JSON Data";
 document.getElementById('head1').innerHTML = "Hotel California";
 document.getElementById('head2').innerHTML = "What I have learned is:";
 
-//get api
-// promises, call backs, asyncs, awaits are similar in ways... please read!
+//getting the api
 async function getHotelData() {
     try {
-        const response = await fetch('js/hotel.JSON')
+        const response = await fetch('js/hotel.json')
         return await response.json() //this returns the JSON objects
 
     }catch (error) {
@@ -18,12 +17,13 @@ async function getHotelData() {
     }
 }
 
+
 let hotelData = {}
 getHotelData().then(data => hotelData = data)
 console.log(hotelData)
 //storing in a variable to document
 let selectAll = document.querySelectorAll('a')
-selectAll.forEach (() => addEventListener('click',hotelInfo))
+selectAll.forEach (() => addEventListener('click', hotelInfo))
 console.log(selectAll)
 //document.querySelector("#marriott").addEventListener('click',hotelInfo)
 
@@ -31,15 +31,14 @@ function hotelInfo(event) {
     let hotelChoice = hotelData.hotels.find(hotel => {
         return event.target.id === hotel.name.toLowerCase()
     })
-
-    //console.log(Event.target.id)
+    console.log(hotelChoice)
 
     document.querySelector('#hotelName').textContent = `${hotelChoice.name} Hotel`
     document.querySelector('#address').textContent = `${hotelChoice.address}`
     document.querySelector('#rooms').textContent = `${hotelChoice.rooms}`
     document.querySelector('#gym').textContent = `${hotelChoice.gym}`
     document.querySelector('#pools').textContent = `${hotelChoice.pools}`
-    document.querySelector('#type').textContent = `${hotelChoice.roomTypes[0] + ' ' + hotelChoice.roomTypes[1] + ' ' + hotelChoice.roomTypes[2] + ' ' + hotelChoice.roomTypes[3]}`
+    document.querySelector('#type').textContent = `${hotelChoice.roomTypes[0] + hotelChoice.roomTypes[1] + hotelChoice.roomTypes[2] + hotelChoice.roomTypes[3]}`
     
     let picturePlacement = document.querySelector('#picture')
     picturePlacement.src = `${hotelChoice.picture}`
