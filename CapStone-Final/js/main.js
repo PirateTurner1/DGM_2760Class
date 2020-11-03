1b36deb994402eb0d8887178d0c99ab2189a7606
austin <55255361+PirateTurner1@users.noreply.github.com>
2020-11-03 03:49:38 -0700

    ?ideas?

--- CapStone-Final/js/main.js
+++ CapStone-Final/js/main.js
@@ -5,3 +5,10 @@ document.querySelector('header > h2').innerText = "Final Project!";
 //giving the body an h1-h2 header.
 document.getElementById('head1').innerHTML = "Title here!";
 document.getElementById('head2').innerHTML = "This page demonstrates:";
+
+
+// i could use a few buttons to conjure up a short story.  
+// i could use dice or something to role out a game with lots of random feed back
+// i could make a hotel of different characters 
+// i could make a game to create the weather pattern.
+//buttons to a worded sentence of query's.
