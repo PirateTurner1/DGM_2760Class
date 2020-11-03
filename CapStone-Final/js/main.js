c7e289c9dc6a289878491d283a522efd9630d7ef
austin <55255361+PirateTurner1@users.noreply.github.com>
2020-11-03 03:01:44 -0700

    started in css, html, and java stuff

---
+++ CapStone-Final/js/main.js
@@ -0,0 +1,7 @@
+// giving the top h1-h2 header.
+document.querySelector('header > h1').innerText = "CapStone!";
+document.querySelector('header > h2').innerText = "Final Project!";
+
+//giving the body an h1-h2 header.
+document.getElementById('head1').innerHTML = "Title here!";
+document.getElementById('head2').innerHTML = "This page demonstrates:";
