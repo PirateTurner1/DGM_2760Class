7a3529b8c961132df6550499d8af9c97ce97e3ed
austin <55255361+PirateTurner1@users.noreply.github.com>
2020-11-18 03:01:48 -0700

    ...work...ing...

--- CapStone-Final/js/main.js
+++ CapStone-Final/js/main.js
@@ -49,73 +49,78 @@ function pirateInfo(event) {
     let picture = document.querySelector('#picture')
 }
 
-const firstRandNum = Math.floor(Math.random() *6) + 1;
-const firstDiceImage =  'images/d' + firstRandNum + '.png';
-const secondRandNum = Math.floor(Math.random() *6) + 1;
-const secondDiceImage =  'images/e' + secondRandNum + '.png';
+//const firstRandNum = Math.floor(Math.random() *6) + 1;
+//const firstDiceImage =  'images/d' + firstRandNum + '.png';
+//const secondRandNum = Math.floor(Math.random() *6) + 1;
+//const secondDiceImage =  'images/e' + secondRandNum + '.png';
 
 
-if (firstRandNum > secondRandNum) {
-    document.querySelector('#feedback').innerHTML = 'the winner is: player 1'
-    giveAward()
-} else if (secondRandNum > firstRandNum) {
-    document.querySelector('#feedback').innerHTML = 'the winner is: the pirate!';
-}else {
-    document.querySelector('#feedback').innerHTML = 'its a draw, try again!';
-}
+//if (firstRandNum > secondRandNum) {
+    //document.querySelector('#feedback').innerHTML = 'the winner is: player 1'
+    //giveAward()
+//} else if (secondRandNum > firstRandNum) {
+    //document.querySelector('#feedback').innerHTML = 'the winner is: the pirate!';
+//}else {
+   // document.querySelector('#feedback').innerHTML = 'its a draw, try again!';
+//}
+
+
+const sides = ["d1.png", "d2.png", "d3.png", "d4.png", "d5.png", "d6.png"];
+const side_alt = ["roll: 1", "roll: 2", "roll: 3", "roll: 4", "roll: 5", "roll: 6"];
 
 //generating random dice role
-function throwDice(){
-    if (firstRandNum > secondRandNum) {
-        document.querySelector('#feedback').innerHTML = 'the winner is: player 1'
-        giveAward()
-    } else if (secondRandNum > firstRandNum) {
-        document.querySelector('#feedback').innerHTML = 'the winner is: the pirate!';
-    }else {
-        document.querySelector('#feedback').innerHTML = 'its a draw, try again!';
+function throwDice() {
+    //create a random integer between 1 and 6
+    var rand1 = Math.floor(Math.random()*6) + 1;
+    var rand2 = Math.floor(Math.random()*6) + 1;
+    var rand3 = Math.floor(Math.random()*6) + 1;
+    var rand4 = Math.floor(Math.random()*6) + 1;
+
+    // Set Image src
+    document.getElementById("playerDice1").src = "images/d" + rand1 + ".png";
+    document.getElementById("playerDice2").src = "images/d" + rand2 + ".png";
+    document.getElementById("pirateDice1").src = "images/e" + rand3 + ".png";
+    document.getElementById("pirateDice2").src = "images/e" + rand4 + ".png";
+
+    // Set Image alt
+    document.getElementById("playerDice1").alt = side_alt[rand1];
+    document.getElementById("playerDice2").alt = side_alt[rand2];
+    document.getElementById("pirateDice1").alt = side_alt[rand3];
+    document.getElementById("pirateDice2").alt = side_alt[rand4];
+
+
+    who_won(rand1,rand2,rand3,rand4);
+ 
+
+    function who_won(rand1, rand2, rand3, rand4) {
+        let player_points = rand1 + rand2 + 2;
+        let pirate_points = rand3 + rand4 + 2;
+        let giffy = winner(player_points, pirate_points);
+        document.getElementById("message").src = "images/" + giffy;
+        document.getElementById("message").alt = giffy;
     }
-    let rand1 = Math.floor(Math.random() * 6) + 1;
-    let rand2 = Math.floor(Math.random() * 6) + 1;
-    let rand3 = Math.floor(Math.random() * 6) + 1;
-    let rand4 = Math.floor(Math.random() * 6) + 1;
 
-    document.getElementById('myDice1').src = "images/d" + rand1 + ".png";
-    document.getElementById('myDice2').src = "images/d" + rand2 + ".png";
-    document.getElementById('pirateDice1').src = "images/e" + rand3 + ".png";
-    document.getElementById('pirateDice2').src = "images/e" + rand4 + ".png";
-
-    //let side_alt = ["roll: 1", "roll: 2", "roll: 3", "roll: 4", "roll: 5", "roll: 6"];
-    //document.getElementById('myDice').alt = side_alt[rand1 - 1]
-    //document.getElementById('myDice').alt = side_alt[rand2 - 1]
-    //document.getElementById('pirateDice').alt = side_alt[rand3 - 1]
-    //document.getElementById('pirateDice').alt = side_alt[rand4 - 1]
-}
-document.querySelectorAll('img')[1].setAttribute('src', firstDiceImage)
-document.querySelectorAll ('img')[2].setAttribute('src', firstDiceImage)
-document.querySelectorAll('img')[3].setAttribute('src', secondDiceImage)
-document.querySelectorAll ('img')[4].setAttribute('src', secondDiceImage)
-
-function who_won(rand1, rand2, rand3, rand4) {
-    let PlayerPoints = rand1 + rand2;
-    let piratePoints = rand3 + rand4;
-    let ribbon = winner (PlayerPoints, piratePoints);
-    document.getElementById('awardImage').src = "images/" + ribbon
-    document.getElementById('awardImage').alt = ribbon;
-}
-
-function winner(player, pirate) {
-    if (player < pirate) {
-        return ribbon;
-    } else if (pirate < player) {
-        return ribbon;
-    }else if (player == pirate) {
-        return ;
+    function winner(player, enemy) {
+        if (player < enemy) {
+            return giveAward();
+        }else if (enemy < player) {
+            return giveAward()
+        }else if (player == enemy) {
+            return giveAward()
+        }
     }
+
+   // const feedback = document.querySelector('#feedback')
+    //if (gamerGuess == correctNumber) {
+        //console.log(`gamerGuess is equal to correctNumber`)
+       // feedback.innerText = 'You are correct! \n You win!'
+       // giveAward()
+
 }
-
-
-
-
+//document.querySelectorAll('img')[1].setAttribute('src', firstDiceImage)
+//document.querySelectorAll ('img')[2].setAttribute('src', firstDiceImage)
+//document.querySelectorAll('img')[3].setAttribute('src', secondDiceImage)
+//document.querySelectorAll ('img')[4].setAttribute('src', secondDiceImage)
 
 
 //giving the function to the show results to the user.
